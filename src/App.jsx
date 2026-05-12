be6ccc65ff62c948cf787e8c715fd758a67cac16
sruthi <sruthisrivaragani@gmail.com>
2026-05-12 21:59:22 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import {BrowserRouter,Routes,Route} from "react-router"
+// import Header from "./components/Header"
+import Home from "./components/Home"
+function App() {
+  
+  return (
+    <>
+      <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,24 @@
+// import {Link} from "react-router"
+// import "./index.css"
+// const Header=()=>{
+//     return (
+//         <>
+//          <header>
+//             <nav><h3>Go Business</h3></nav>
+//             <div>
+//             <ul className="nav-links">
+//                 <Link to="/"><li>Home</li></Link>
+//                 <Link to="/about"><li>About Us</li></Link>
+//                 <Link to="/courses"><li>Courses</li></Link>
+//                 <Link to="/projects"><li>Projects</li></Link>
+//                 <Link to="/contact"><li>Contact</li></Link>
+//             </ul>
+//             </div>
+//             <div>
+//                 <button>Try For Free</button>
+//             </div>
+//          </header>
+//         </>
+//     )
+// }
+// export default Header
